e24d446118193bf6427b01932cce0b3be41b2657
Neso Milanovic <nesom993@gmail.com>
2019-12-25 04:51:57 +0100

    prvi deo zadatka

--- blog.sql
+++ blog.sql
@@ -1,71 +1,41 @@
--- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
---
--- Host: localhost    Database: vivify
--- ------------------------------------------------------
--- Server version	5.7.28-0ubuntu0.16.04.2
+select * from posts right join comments on posts.id=comments.post_id where posts.id=1;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- create database blog;
+-- use blog;
 
---
--- Current Database: `vivify`
---
+-- create table posts (
+-- id int auto_increment not null,
+-- title varchar(100),
+-- body varchar(1000),
+-- author varchar(100),
+-- created_at datetime,
+-- primary key(id));
+-- create table comments (
+-- id int auto_increment not null,
+-- author varchar(100),
+-- text varchar(1000),
+-- post_id int,
+-- created_at datetime,
+-- primary key(id),
+-- foreign key (post_id) references posts(id));
+-- insert into posts (title, body, author, created_at) values ("Najnovija odeca u modi!", "Ovog leta popularne su duze suknje, kako za dame, tako i za mladju populaciju", "Mika", '2001-01-01 01:01:01');
+-- insert into posts (title, body, author, created_at) values ("Popularni automobili 2002.", "Medju popularnijim automobilima 2002. izdvaja se GOLF 4!", "Zika", '2002-02-02 02:02:02');
+-- insert into posts (title, body, author, created_at) values ("2003. godina preokreta!?", "Ova godina prema verovanju budista donosi veliki preokret u vasem zivotu!", "Pera", '2003-03-03 03:03:03');
+-- insert into comments (author, text, post_id, created_at) values ("Ceca", "Stvarno su super suknje!", 1, '2001-01-03 03:03:30');
+-- insert into comments (author, text, post_id, created_at) values ("Bobana", "Bela je najbolja!", 1, '2001-01-02 02:02:20');
+-- insert into comments (author, text, post_id, created_at) values ("Andrea", "Bas bih zelela jednu ovakvu!", 1, '2001-01-01 01:01:10');
+-- insert into comments (author, text, post_id, created_at) values ("Stevica", "Golf GTI najbolji auto!", 2, '2002-01-03 03:03:30');
+-- insert into comments (author, text, post_id, created_at) values ("Mile", "I ja imam ovakvog zutog!", 2, '2002-01-02 02:02:20');
+-- insert into comments (author, text, post_id, created_at) values ("Aleksa", "Potvrdjujem za Golfa, stvarno odlican auto", 2, '2002-01-01 01:01:10');
+-- insert into comments (author, text, post_id, created_at) values ("Jovana", "Nadam se! :)", 3, '2003-01-03 03:03:30');
+-- insert into comments (author, text, post_id, created_at) values ("Ljubica", "Zar ne treba da bude smak sveta?", 3, '2003-01-02 02:02:20');
+-- insert into comments (author, text, post_id, created_at) values ("Andjelka", "Ja ne verujem u ove gluposti!", 3, '2003-01-01 01:01:10');
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `vivify` /*!40100 DEFAULT CHARACTER SET latin1 */;
+describe comments; 
+select * from comments;
+describe posts;
+select * from posts;
 
-USE `vivify`;
 
---
--- Table structure for table `akademci`
---
 
-DROP TABLE IF EXISTS `akademci`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `akademci` (
-  `id` int(11) DEFAULT NULL,
-  `ime` varchar(34) DEFAULT NULL,
-  `prezime` varchar(34) DEFAULT NULL,
-  `jmbg` char(13) DEFAULT NULL,
-  `datum_rodjenja` date DEFAULT NULL,
-  `postanski_kod` char(5) DEFAULT NULL,
-  `pol` char(5) DEFAULT NULL,
-  `grad` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `akademci`
---
-
-LOCK TABLES `akademci` WRITE;
-/*!40000 ALTER TABLE `akademci` DISABLE KEYS */;
-/*!40000 ALTER TABLE `akademci` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Current Database: `blog`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `blog` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `blog`;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-12-24 21:20:01
--- single-post.php
+++ single-post.php
@@ -1,5 +1,139 @@
 <?php
-    include('header.php');
-    include('sidebar.php');
-    include('footer.php');
+    // ako su mysql username/password i ime baze na vasim racunarima drugaciji
+    // obavezno ih ovde zamenite
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "blog";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Vivify Blog</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="styles/blog.css" rel="stylesheet">
+    <link href= "styles/styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include('header.php'); ?>
+
+
+<?php
+                if (isset($_GET['posts_id'])) {
+
+                    // pripremamo upit
+                    $sql = "SELECT * from posts WHERE posts.id= {$_GET['posts_id']}";
+                    $statement = $connection->prepare($sql);
+
+                    // izvrsavamo upit
+                    $statement->execute();
+
+                    // zelimo da se rezultat vrati kao asocijativni niz.
+                    // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+                    $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+                    // punimo promenjivu sa rezultatom upita
+                    $post = $statement->fetch();
+
+                    // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+                        echo '<pre>';
+                        var_dump($post);
+                        echo '</pre>';                    
+
+            ?>
+
+<?php
+                            
+                            //DOBAVLJAMO KOMENTARE ZA OGLAS
+                            // pripremamo upit
+                            $sql = "SELECT * from comments where post_id = {$_GET['posts_id']} order by created_at DESC";
+                            $statement = $connection->prepare($sql);
+
+                            // izvrsavamo upit
+                            $statement->execute();
+
+                            // zelimo da se rezultat vrati kao asocijativni niz.
+                            // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+                            $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+                            // punimo promenjivu sa rezultatom upita
+                            $comments = $statement->fetchAll();
+
+                            // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+                                echo '<pre>';
+                                var_dump($comments);
+                                echo '</pre>';
+
+                    ?>
+
+<?php include('sidebar.php'); ?>
+
+<div class="blog-post">
+                <h2 class="blog-post-title"><?php echo $post['title']?></h2>
+                <p class="blog-post-meta"><?php echo $post['created_at']?> by <a href="#"><?php echo $post['author']?></a></p>
+
+                <p><?php echo $post['body']?></p>
+                
+            </div>
+
+            
+                            <div class="single-comment">
+                            <?php foreach ($comments as $comment) { ?>
+                            <ul type = "none">
+                                <li>
+                                    <?php echo $comment['text'];?> <br>  
+                                        <i>posted by:</i> <strong><?php echo $comment['author'];?><hr class="comment-ruller"></strong>
+                                </li>  
+                            </ul> 
+                             <?php }?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+                } else {
+                    echo('post_id nije prosledjen kroz $_GET');
+                }
+            ?>
+
+<?php include ('footer.php'); ?>
+</body>
+</html>
