3d547d99ef74dd0a0cb0ec849caaa53a95833fa7
Neso Milanovic <nesom993@gmail.com>
2019-12-26 21:11:56 +0100

    db added

--- blog.sql
+++ blog.sql
@@ -31,18 +31,8 @@ CREATE TABLE `comments` (
   PRIMARY KEY (`id`),
   KEY `post_id` (`post_id`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `comments`
---
-
-LOCK TABLES `comments` WRITE;
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,'Ceca','Stvarno su super suknje!',1,'2001-01-03 03:03:30'),(2,'Bobana','Bela je najbolja!',1,'2001-01-02 02:02:20'),(3,'Andrea','Bas bih zelela jednu ovakvu!',1,'2001-01-01 01:01:10'),(4,'Stevica','Golf GTI najbolji auto!',2,'2002-01-03 03:03:30'),(5,'Mile','I ja imam ovakvog zutog!',2,'2002-01-02 02:02:20'),(6,'Aleksa','Potvrdjujem za Golfa, stvarno odlican auto',2,'2002-01-01 01:01:10'),(7,'Jovana','Nadam se! :)',3,'2003-01-03 03:03:30'),(8,'Ljubica','Zar ne treba da bude smak sveta?',3,'2003-01-02 02:02:20'),(9,'Andjelka','Ja ne verujem u ove gluposti!',3,'2003-01-01 01:01:10');
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `posts`
@@ -60,16 +50,6 @@ CREATE TABLE `posts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `posts`
---
-
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,'Najnovija odeca u modi!','Ovog leta popularne su duze suknje, kako za dame, tako i za mladju populaciju','Mika','2001-01-01 01:01:01'),(2,'Popularni automobili 2002.','Medju popularnijim automobilima 2002. izdvaja se GOLF 4!','Zika','2002-02-02 02:02:02'),(3,'2003. godina preokreta!?','Ova godina prema verovanju budista donosi veliki preokret u vasem zivotu!','Pera','2003-03-03 03:03:03');
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -80,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-26 19:49:30
+-- Dump completed on 2019-12-26 21:11:13
